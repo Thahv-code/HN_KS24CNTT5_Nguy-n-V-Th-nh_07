55bd0c6edcc60fbfd2508ef9c07b9ebad278f24d
Thahv-code <vuthanhnguyen946@gmail.com>
2025-09-05 17:32:29 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,11 @@
-import { Flex } from "antd";
 import "./App.css";
-import Inputform from "./compoment/input";
-import OpenModal from "./compoment/modal";
+import BlogApp from "./compoment/Filter";
 
 function App() {
   return (
-    <div class="bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 ...">
-      <div class="top">
-
-      </div>
-      <div class="middle">
-        <p>Lọc theo</p>
-        <select class="...">
-          <option>1</option>
-          <option>2</option>
-          <option>3</option>
-        </select>
-        <OpenModal>
-          <Inputform />
-        </OpenModal>
-      </div>
-      <div class="bottom">
-        
-  </div>
-
-    </div>
+    <>
+      <BlogApp />
+    </>
   );
 }
 
